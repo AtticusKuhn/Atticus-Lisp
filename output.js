a639b9f09fa6e3ead6142981223bb20166774012
eulerthedestroyer <eulerthedestroyer@gmail.com>
2022-03-24 20:15:51 -0700

    work on ocdegen

--- output.js
+++ output.js
@@ -1,39 +1,32 @@
 
-const doesPatternMatch = () => {
-       return true
+const alispNamespace = {};
+const func = function () {
+       const blankFunc = () => null;
+       blankFunc.patternMatch = function (patternMatch, body) {
+              const old = this;
+              const newFunc = function (...args) {
+
+              }
+              return newFunc
+       }
+       return blankFunc;
 }
-const error = (e) => {
-       throw new Error(e)
-}
-
-const alispNamespace = {}
-
-alispNamespace["<"] = (a_0) => (a_1) => {
-       return doesPatternMatch([a, b], [a_0, a_1])
-              ? a < b :
-              error("no pattern matched");
-};
-alispNamespace["+"] = (a_0) => (a_1) => {
-       return doesPatternMatch([a, b], [a_0, a_1])
-              ? a + b :
-              error("no pattern matched");
-};
-alispNamespace["-"] = (a_0) => (a_1) => {
-       return doesPatternMatch([a, b], [a_0, a_1])
-              ? a - b :
-              error("no pattern matched");
-};
-alispNamespace["if"] = (a_0) => (a_1) => (a_2) => {
-       return doesPatternMatch([Symbol("true"), a, b], [a_0, a_1, a_2])
-              ? alispNamespace["a"]() : doesPatternMatch([Symbol("false"), a, b], [a_0, a_1, a_2])
-                     ? alispNamespace["b"]() :
-                     error("no pattern matched");
-};
-alispNamespace["main"] = () => {
-       return doesPatternMatch([], [])
-              ? alispNamespace["if"](Symbol("true"), true, false) :
-              error("no pattern matched");
-};
-console.log(alispNamespace["main"]()()())
-console.log("hi")
-
+alispNamespace["<"] = func().patternMatch([{ type: "variable", value: "a" }, { type: "variable", value: "b" }], function (...args) {
+       return alispNamespace["a"] < alispNamespace["b"]
+});
+alispNamespace["+"] = func().patternMatch([{ type: "variable", value: "a" }, { type: "variable", value: "b" }], function (...args) {
+       return alispNamespace["a"] + alispNamespace["b"]
+});
+alispNamespace["-"] = func().patternMatch([{ type: "variable", value: "a" }, { type: "variable", value: "b" }], function (...args) {
+       return alispNamespace["a"] - alispNamespace["b"]
+});
+alispNamespace["if"] = func().patternMatch([Symbol("true"), { type: "variable", value: "a" }, { type: "variable", value: "b" }], function (...args) {
+       return alispNamespace["a"]
+})
+       .patternMatch([Symbol("false"), { type: "variable", value: "a" }, { type: "variable", value: "b" }], function (...args) {
+              return alispNamespace["b"]
+       });
+alispNamespace["main"] = func().patternMatch([], function (...args) {
+       return alispNamespace["if"](Symbol("true"))(true)(false)
+});
+;; alispNamespace["main"]()
--- src/compiler/codegen/javascript.ts
+++ src/compiler/codegen/javascript.ts
@@ -1,4 +1,4 @@
-import { ASTtoProgram, parse, program, value } from "../parser/parser";
+import { ASTtoProgram, parse, program, value, func } from "../parser/parser";
 import fs from "fs"
 const codeGenValueToJavascript = (value: value): string => {
     switch (value.type) {
@@ -11,8 +11,10 @@ const codeGenValueToJavascript = (value: value): string => {
             const rst = value.values.slice(1)
             if (fst.type === "identifier") {
                 if (fst.value === "compile")
-                    return codeGenValueToJavascript(rst[0])
-                return `alispNamespace["${fst.value}"](${rst.map(a => `(${codeGenValueToJavascript(a)})`).join(",")})`
+                    return codeGenValueToJavascript(rst[0]).replace(/\$([^\s]+)/g, (match) => {
+                        return `alispNamespace["${match.substring(1)}"]`
+                    })
+                return `alispNamespace["${fst.value}"]${rst.map(a => `(${codeGenValueToJavascript(a)})`).join("")}`
             } else {
                 return `[${value.values.map(codeGenValueToJavascript).join(", ")}]`
             }
@@ -34,9 +36,9 @@ export const codegenToJavascript = (program: program): string => {
     let main = ""
     for (const [name, funcs] of program.names.entries()) {
         let funstring = ` alispNamespace["${name}"] = `
-        funstring += `${funcs[0].arguments.map((_, i) => `(a_${i})=>`).join(" ")} `
+        funstring += funcs[0].arguments.length > 0 ? `${funcs[0].arguments.map((_, i) => `(a_${i})=>`).join(" ")} ` : "()=>"
         funstring += `{
-                    `
+         console.log("${name} called");          `
         funstring += `return ` +
             `${funcs.map(f => `doesPatternMatch([${f.arguments.map(codeGenValueToJavascript).join(", ")}], [${f.arguments.map((_, i) => `a_${i}`).join(", ")}]) 
         ? ${codeGenValueToJavascript(f.body)} : `).join(" ")
@@ -51,18 +53,35 @@ export const codegenToJavascript = (program: program): string => {
                 ${main}
                 `
 }
-const runtime = `
-                const doesPatternMatch = () => {
-                    return true
-                }
-                const error = (e) => {
-                    throw new Error(e)
-                }
-                alispNamespace["main"]()
-                    `
+const genPatternMatch = (value: value): string =>
+    value.type === "identifier" ? `{type:"variable", value:"${value.value}"}`
+        : value.type === "number_literal" ? value.value.toString()
+            : value.type === "keyword_symbol" ? `Symbol("${value.value}")`
+                : value.type === "string_literal" ? `"${value.value}"`
+                    : value.type === "sExpression" ? "idk"
+                        : "idk2"
+const mapDefs = (func: func): string => {
+    return `.patternMatch([${func.arguments.map(genPatternMatch).join(", ")}], function(...args){
+        return ${codeGenValueToJavascript(func.body)}
+    })`
+}
+const codegenToJavascript2 = (program: program): string => {
+    let main = ""
+    for (const [name, funcs] of program.names.entries()) {
+        main += `alispNamespace["${name}"] = func() ${funcs.map(mapDefs).join("\n")};\n`
+    }
+    return `
+    const alispNamespace = {};
+    const func = ()=>{
+
+    }
+    ${main};`
+}
+
 export const JSCodeGen = (soureCode: string): string => {
     const stdlib = fs.readFileSync("./src/examples/stdlib.alisp", "utf-8");
     const res = parse(stdlib + "\n" + soureCode)
     const program = ASTtoProgram(res)
-    return runtime + codegenToJavascript(program)
+    return codegenToJavascript2(program) + `;             alispNamespace["main"]()
+    `
 }
